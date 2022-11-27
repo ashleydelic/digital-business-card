74b0225a53fbd310b2c597c3cdf06c267e0699fb
ashleydelic <ashleycohzer96@gmail.com>
2022-11-26 21:06:56 -0500

    email and linkedin icons

--- src/Main.js
+++ src/Main.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Main() {
   return (
     <div className='main-container'>
-      <h1 className='my-name'>Ashley Delic</h1>
-      <h2 className='profession-title'>Frontend Developer</h2>
+      <h1>Ashley Delic</h1>
+      <h2>Frontend Developer</h2>
       <a href='ashley.website'>ashley.website</a>
       <div className='eemail-btn'>
       <a href='mailto:ashleycohzer96@gmail.com'></a><button>Email</button>
