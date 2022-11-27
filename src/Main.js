431d70748a58fbd5934d06c293e06206497b22ed
ashleydelic <ashleycohzer96@gmail.com>
2022-11-26 21:41:41 -0500

    css update

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from 'react';
+import emailIcon from './email-icon.png';
+import linkedInIcon from './linkedin-icon.png';
+
 
 export default function Main() {
   return (
     <div className='main-container'>
       <h1>Ashley Delic</h1>
+
       <h2>Frontend Developer</h2>
+
+      <h3 className='website'>
       <a href='ashley.website'>ashley.website</a>
-      <div className='eemail-btn'>
-      <a href='mailto:ashleycohzer96@gmail.com'></a><button>Email</button>
+      </h3>
+
+      <div className='email-btn'>
+      <button><img src={emailIcon} />Email</button>
       </div>
+
       <div className='linkedin-btn'>
-          <a href='linkedin.com/in/ashleydelic'></a><button>LinkedIn</button>
+          <button><img src={linkedInIcon} />LinkedIn</button>
+      </div>
+
+      <div className='About-section'>
+        <h2>About</h2>
+        <p></p>
       </div>
     </div>
   )
